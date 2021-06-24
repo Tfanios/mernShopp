6a7242352c12da86044d1b427a8bd4cff14f4dec
Tfanios <63114358+Tfanios@users.noreply.github.com>
2021-06-24 19:20:08 +0300

    Update api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -7,7 +7,7 @@ axios.defaults.withCredentials = false
 
 //PRODUCT 
 export const createProduct = (newProduct)=> axios.post(`${url}/product/new`, newProduct);
-export const fetchProducts = () => axios.get(`${url}/product`)
-export const fetchSingleProduct = (id) => axios.get(`${url}/product/${id}`)
+export const fetchProducts = () => axios.get(`${productionURL}/product`)
+export const fetchSingleProduct = (id) => axios.get(`${productionURL}/product/${id}`)
 export const editProduct = (id, editedProduct) => axios.put(`${url}/product/${id}/edit`,editedProduct)
 export const deleteProduct = (id) => axios.delete(`${url}/product/${id}/delete`)
